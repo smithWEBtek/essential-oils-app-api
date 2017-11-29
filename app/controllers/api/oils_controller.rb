82dc995b5f06610287288fbee03dc996ff41a982
mcclains2003 <mcclains2003@gmail.com>
2017-11-29 11:50:23 -0600

    Completed api/oils controller actions

--- app/controllers/api/oils_controller.rb
+++ app/controllers/api/oils_controller.rb
@@ -2,22 +2,40 @@ class API::OilsController < ApplicationController
   before_action :set_oil, only: [:show, :update, :destroy]
 
   def index
-      @oils = Oil.all
+    render json: Oil.all
+  end
 
-      render json: @oils
+  def show
+    render json: @oil
   end
 
   def create
     @oil = Oil.new(oil_params)
-
+    
     if @oil.save
         render json: @oil
     else
         render_errors_in_json
     end
-end
+  end
 
-private
+  def update
+    if @oil.update(oil_params)
+        render json: @oil
+    else
+        render_errors_in_json
+    end
+  end
+
+  def destroy
+    if @oil.destroy
+      render status: 204
+    else
+      render json: { message: "Unable to Remove this Oil!" }, status:400
+    end
+  end
+
+  private
 
   def oil_params
       params.require(:oil).permit(:name, :description, :uses, :fragrance_profile, :medical_properties)
@@ -40,7 +58,54 @@ private
           errors: { 
               messages: @oil.errors.messages
           }
-      }
+      }, status: 422
   end
 
 end
+
+# class API::OilsController < ApplicationController
+#   before_action :set_oil, only: [:show, :update, :destroy]
+
+#   def index
+#       @oils = Oil.all
+
+#       render json: @oils
+#   end
+
+#   def create
+#     @oil = Oil.new(oil_params)
+
+#     if @oil.save
+#         render json: @oil
+#     else
+#         render_errors_in_json
+#     end
+#   end
+
+# private
+
+#   def oil_params
+#       params.require(:oil).permit(:name, :description, :uses, :fragrance_profile, :medical_properties)
+#   end
+
+#   def set_oil
+#       @oil = Oil.find_by(id: params[:id])
+
+#       if !@oil
+#           render json: { 
+#               errors: { 
+#                   messages: { oil: "Oil can't be found" }
+#               }
+#           }, status: 404
+#       end
+#   end
+
+#   def render_errors_in_json
+#       render json: { 
+#           errors: { 
+#               messages: @oil.errors.messages
+#           }
+#       }
+#   end
+
+# end
