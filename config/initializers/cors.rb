b84d0724d3e63a141ee4e593c83a608c2c2ff7dd
mcclains2003 <mcclains2003@gmail.com>
2017-12-01 08:44:03 -0600

    Added another origin point to the cors file, removed notes from it.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,17 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins ['localhost:3000', 'localhost:3001']
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
-
-
-# t.string   "name"
-# t.text     "description"
-# t.text     "uses"
-# t.text     "fragrance_profile"
-# t.text     "medical_properties"
